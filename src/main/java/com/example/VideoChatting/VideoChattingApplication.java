6cff3ae2d9974b4e7adfcda7eb323fb9d6f3a364
LminWoo99 <mw310@naver.com>
2023-10-04 18:49:50 +0900

    first commit

---
+++ src/main/java/com/example/VideoChatting/VideoChattingApplication.java
@@ -0,0 +1,13 @@
+package com.example.VideoChatting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoChattingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VideoChattingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/VideoChatting/VideoChattingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VideoChatting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoChattingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
