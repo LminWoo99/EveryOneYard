feb7da430d84ad334a537e8e1e41de86715845b0
LminWoo99 <mw310@naver.com>
2023-10-05 22:03:23 +0900

    release 0.0.2

--- src/main/java/com/example/VideoChatting/controller/HelloController.java
+++ src/main/java/com/example/VideoChatting/controller/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @GetMapping("")
     public String hello() {
-        return "hello ec2 & docker";
+        return "hello ec2 & docker ver2";
     }
 }
