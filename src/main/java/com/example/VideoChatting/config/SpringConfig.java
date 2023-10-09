e68d420e12864f78c1fd452452faa7fd28523599
LminWoo99 <mw310@naver.com>
2023-10-09 12:50:43 +0900

    Feat : Add Chatting 일반 채팅 브랜치 생성

---
+++ src/main/java/com/example/VideoChatting/config/SpringConfig.java
@@ -0,0 +1,9 @@
+package com.example.VideoChatting.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class SpringConfig implements {
+}
