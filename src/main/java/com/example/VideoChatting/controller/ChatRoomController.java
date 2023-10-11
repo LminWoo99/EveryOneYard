db87e888f0a99a21ce7bbbd4855fdf5170535dbb
LminWoo99 <mw310@naver.com>
2023-10-11 23:09:00 +0900

    Feat : Add File chat 채팅 파일 첨부기능 추가, 현재 front 이슈있음 Fixes

--- src/main/java/com/example/VideoChatting/controller/ChatRoomController.java
+++ src/main/java/com/example/VideoChatting/controller/ChatRoomController.java
@@ -5,12 +5,12 @@ import com.example.VideoChatting.service.chat.ChatRoomService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
-
-@RestController
+@Controller
 @Slf4j
 @RequiredArgsConstructor
 @RequestMapping("/chat")
@@ -19,21 +19,22 @@ public class ChatRoomController {
 
     @GetMapping("/")
     @ApiOperation(value = "채팅방 리스트 조회 ", notes = "전체 채팅방 목록을 조회한다")
-    public ResponseEntity<List<ChatRoom>> chatRoomList() {
-        List<ChatRoom> allRooms = chatRoomService.findAllRooms();
-        return ResponseEntity.ok().body(allRooms);
+    public String chatRoomList(Model model) {
+        model.addAttribute("list",chatRoomService.findAllRooms());
+        return "roomList";
     }
     @PostMapping("/createRoom")
     @ApiOperation(value = "채팅방 생성 ", notes = "채팅방을 생성한다")
-    public ResponseEntity<ChatRoom> createRoom(@RequestParam String name) {
-        ChatRoom chatRoom = chatRoomService.createChatRoom(name);
-        return ResponseEntity.ok().body(chatRoom);
+    public String createRoom(@RequestParam String roomName, RedirectAttributes rttr) {
+        ChatRoom room = chatRoomService.createChatRoom(roomName);
+        rttr.addFlashAttribute("roomName", room);
+        return "redirect:/chat/";
     }
     @GetMapping("/{roomId}")
     @ApiOperation(value = "채팅방 입장 ", notes = "파라미터로 넘어오는 ROOMID 기준으로 채팅방을 찾음 ")
-    public ResponseEntity<ChatRoom> roomDetail(@PathVariable("roomId") String roomId){
-        return ResponseEntity.ok().body(chatRoomService.findRoomById(roomId));
-
+    public String roomDetail(Model model, String roomId){
+        model.addAttribute("room", chatRoomService.findRoomById(roomId));
+        return "chatroom";
     }
 
 }
---
+++ src/main/java/com/example/VideoChatting/controller/FileController.java
@@ -0,0 +1,43 @@
+package com.example.VideoChatting.controller;
+
+import com.example.VideoChatting.dto.FileUploadDto;
+import com.example.VideoChatting.service.file.S3FileService;
+import io.swagger.annotations.ApiOperation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/s3")
+@RequiredArgsConstructor
+public class FileController {
+
+    private final S3FileService fileService;
+
+    // 프론트에서 ajax 를 통해 /upload 로 MultipartFile 형태로 파일과 roomId 를 전달받는다.
+    // 전달받은 file 를 uploadFile 메서드를 통해 업로드한다.
+    @PostMapping("/upload")
+    @ApiOperation(value = "채팅시 파일 업로드 ", notes = "MultipartFile 형태로 파일과 roomId 를 전달받고 s3에 업로드")
+    public FileUploadDto uploadFile(@RequestParam("file") MultipartFile file, @RequestParam("roomId") String roomId) {
+        FileUploadDto fileReq = fileService.uploadFile(file, UUID.randomUUID().toString(), roomId);
+        return fileReq;
+    }
+    // get 으로 요청이 오면 아래 download 메서드를 실행한다.
+    // fileName 과 파라미터로 넘어온 fileDir 을 getObject 메서드에 매개변수로 넣는다.
+    @GetMapping("/download/{fileName}")
+    @ApiOperation(value = "채팅시 파일 다운로드 ", notes = "파일 다운로드")
+    public ResponseEntity<byte[]> downlaod(@PathVariable String fileName, @RequestParam("fileDir") String fileDir) {
+        try {
+            // 변환된 byte, httpHeader 와 HttpStatus 가 포함된 ResponseEntity 객체를 return 한다.
+            return fileService.getObject(fileDir, fileName);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    }
+
