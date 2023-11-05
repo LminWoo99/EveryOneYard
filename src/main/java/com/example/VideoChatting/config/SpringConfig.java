408964c7c09c1953dd594f2f8ae54fdcdb3962da
LminWoo99 <mw310@naver.com>
2023-11-05 22:18:10 +0900

    Fix : SpringConfig allow origins websocket cors 허용을 위해 springconfig 수정

--- src/main/java/com/example/VideoChatting/config/SpringConfig.java
+++ src/main/java/com/example/VideoChatting/config/SpringConfig.java
@@ -5,6 +5,8 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+import org.springframework.web.socket.server.support.HttpSessionHandshakeInterceptor;
 
 @Configuration
 @EnableWebSocketMessageBroker
@@ -13,6 +15,9 @@ public class SpringConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // stomp 접속 주소 url => /ws-stomp
         registry.addEndpoint("/ws-stomp") // 연결될 엔드포인트
+                .setAllowedOrigins("*") // 모든 도메인에서 접근 허용 (* 대신 특정 도메인을 지정할 수도 있습니다)
+                .setHandshakeHandler(new DefaultHandshakeHandler())
+                .addInterceptors(new HttpSessionHandshakeInterceptor())
                 .withSockJS(); // SocketJS 를 연결한다는 설정
     }
 
