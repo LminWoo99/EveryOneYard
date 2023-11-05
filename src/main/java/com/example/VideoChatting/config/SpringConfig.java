fdfd5011943d8cf10720621cda2cac6c66b5a979
LminWoo99 <mw310@naver.com>
2023-11-05 22:50:40 +0900

    Fix : security issue same origin patterns

--- src/main/java/com/example/VideoChatting/config/SpringConfig.java
+++ src/main/java/com/example/VideoChatting/config/SpringConfig.java
@@ -15,7 +15,7 @@ public class SpringConfig implements WebSocketMessageBrokerConfigurer {
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // stomp 접속 주소 url => /ws-stomp
         registry.addEndpoint("/ws-stomp") // 연결될 엔드포인트
-                .setAllowedOrigins("*") // 모든 도메인에서 접근 허용 (* 대신 특정 도메인을 지정할 수도 있습니다)
+                .setAllowedOriginPatterns("https://everyoneyard.shop/") // 모든 도메인에서 접근 허용 (* 대신 특정 도메인을 지정할 수도 있습니다)
                 .setHandshakeHandler(new DefaultHandshakeHandler())
                 .addInterceptors(new HttpSessionHandshakeInterceptor())
                 .withSockJS(); // SocketJS 를 연결한다는 설정
